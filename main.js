ab7808d7bf7548fb45976cd7fe19e7cfb8b12cbe
jihad khorfan <jihad.kherfan@gmail.com>
2017-04-03 19:39:05 +0200

    disable developer tools

--- main.js
+++ main.js
@@ -153,7 +153,7 @@ app.on('ready', function () {
     maximizable: false,
     show: false
   });
-  newEvaluation.openDevTools();
+  //newEvaluation.openDevTools();
   newEvaluation.setMenu(null);
   newEvaluation.loadURL(newEvaluationPath);
   newEvaluation.on('close', function (event) {
