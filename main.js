03d4cf3961498ca0ad63adf50a8d6cdd30db415c
jihad.kherfan <spyshow86@gmail.com>
2017-05-20 15:56:42 +0200

    fixed update TFS report

--- main.js
+++ main.js
@@ -24,10 +24,7 @@ const {
 } = require('electron');
 const async = require('async');
 const sql = require('mssql');
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-const { window } = new JSDOM(`<!DOCTYPE html>`);
-const $ = require('jQuery')(window);
+
 
 
 //======================================================================================================================
